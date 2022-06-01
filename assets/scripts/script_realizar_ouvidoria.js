9fd5cbe2f8de69a6d3fdbac418eccfb4e44a009b
AdrianoMaia <drikomaia89@gmail.com>
2022-06-01 10:29:21 -0300

    Começando o js nas páginas

---
+++ assets/scripts/script_realizar_ouvidoria.js
@@ -0,0 +1,17 @@
+
+
+function enviar(){
+    var nome = document.getElementById("nome").value;
+    var cpf = document.getElementById("cpf").value;
+    var email = document.getElementById("email").value;
+    var data = document.getElementById("nasc").value;
+    var sex_M = document.querySelector('input[name="sex"]:checked').value;
+    var sex_F = document.querySelector('input[name="sex"]:checked').value;
+    
+    if(!nome || !cpf || !email){
+        alert("Preenche todos os campos abaixo");
+    }else{
+        alert(`Solicitação enviada!
+Sr(a). ${nome} seu protocolo foi enviado para seu e-mail.`);
+    }
+}
