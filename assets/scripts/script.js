1fd55dfeebdfcd256c8339e4e7510bf7d1f493ca
AdrianoMaia <drikomaia89@gmail.com>
2022-06-01 09:30:09 -0300

    Form finalizado

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,4 +1,14 @@
 function alerta(){
-    alert(`Informações enviadas!
-Chegue seu e-mail.`)
+    var nome = document.getElementById("nome").value;
+    var cpf = document.getElementById("cpf").value;
+    var email = document.getElementById("email").value;
+    var data = document.getElementById("nasc").value;
+    var sex_M = document.querySelector('input[name="sex"]:checked').value;
+    var sex_F = document.querySelector('input[name="sex"]:checked').value;
+    
+    if(!nome || !cpf || !email){
+        alert("Preenche todos os campos abaixo");
+    }
+    
+
 }
