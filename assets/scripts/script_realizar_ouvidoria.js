45ce4d2c35ec6d091e8bdee5086642c4364a6159
AdrianoMaia <drikomaia89@gmail.com>
2022-06-02 13:46:52 -0300

    Adicionando select option em tela de nova ouvidoria

--- assets/scripts/script_realizar_ouvidoria.js
+++ assets/scripts/script_realizar_ouvidoria.js
@@ -7,8 +7,9 @@ function enviar(){
     var data = document.getElementById("nasc").value;
     var sex_M = document.querySelector('input[name="sex"]:checked').value;
     var sex_F = document.querySelector('input[name="sex"]:checked').value;
+    var mensagem = document.getElementById("mensagem").value;
     
-    if(!nome || !cpf || !email){
+    if(!nome || !cpf || !email || !mensagem){
         alert("Preenche todos os campos abaixo");
     }else{
         alert(`Solicitação enviada!
